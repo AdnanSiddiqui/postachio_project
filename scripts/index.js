bed9bc9745aff7ce77365ec253b80e5dc95bba06
Charliebrwn1209 <charliebrwn1209@gmail.com>
2013-09-12 11:10:38 -0400

    modals appear and diappear with style

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,57 @@ var tagline = document.getElementById('tagline');
 var extra = document.getElementById('extra');
 
 tagline.addEventListener('click', function() {
-	extra.classList.toggle('shown');
+	extra.classList.toggle('extra-shown');
 }, false);
 
-var close = document.getElementById('extra-close');
+var extraClose = document.getElementById('extra-close');
 
-close.addEventListener('click', function() {
-	extra.classList.remove('shown');
-	extra.classList.add('disappear');
+extraClose.addEventListener('click', function() {
+	extra.classList.remove('extra-shown');
+	extra.classList.add('extra-disappear');
 	var timeoutId = window.setTimeout(function() {
-		extra.classList.remove('disappear');
+		extra.classList.remove('extra-disappear');
 	}, 2000);
 }, false);
 
-var second = document.querySelectorAll('.modal')[1];
-second.style.visibility = "hidden";
+var seeker = document.querySelector('#seeker'),
+	storyteller = document.querySelector('#storyteller'),
+	resume = document.querySelector('.modal#resume'),
+	samples = document.querySelector('.modal#samples');
+
+seeker.addEventListener('click', function() {
+	resume.classList.add('appear');
+}, false);
+
+storyteller.addEventListener('click', function() {
+	samples.classList.add('appear');
+}, false);
+
+var modalClose = document.querySelectorAll('.close');
+
+var i,
+	length = modalClose.length;
+
+for( i = 0; i < length; i++ ) {
+	modalClose[i].addEventListener('click', function() {
+		if (resume.className == "modal appear" ) {
+			console.log('true');
+			resume.classList.remove('appear');
+			resume.classList.add('disappear');
+			var timeoutId = window.setTimeout(function() {
+				resume.classList.remove('disappear');
+			}, 1500);			
+		}
+
+		samples.classList.remove('appear');
+		samples.classList.add('disappear');
+		var timeoutId = window.setTimeout(function() {
+			samples.classList.remove('disappear');
+		}, 1500);
+	}, false);
+}
+
+var footer = document.getElementsByTagName('footer')[0];
+
+
+
